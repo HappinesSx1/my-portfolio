9c2aafd37ab251ee18480ea261fadb7e922fdea3
unknown <david.b57970@laposte.net>
2023-06-09 03:19:26 +0200

    First page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div className="head">
-        <h1>Bonjour, bienvenue sur mon Portfolio !</h1>
+      <div className="hero">
+        <div className="left-hero">
+          <h1>Bonjour, bienvenue sur mon Portfolio !</h1>
+        </div>
+        <div className="right-hero">
+          <img src="./Ordinateur.png" alt="ordinateur" />
+        </div>
       </div>
     </header>
   );
